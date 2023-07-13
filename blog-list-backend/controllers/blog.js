ed36f12e62216fa6e6b21d8e12ee97b70de0c9c4
nikogenix <nikogenix@gmail.com>
2023-07-13 04:15:52 +0300

    Adding middleware for finding a single blog

--- blog-list-backend/controllers/blog.js
+++ blog-list-backend/controllers/blog.js
@@ -2,15 +2,19 @@ const router = require("express").Router();
 
 const { Blog } = require("../models");
 
+const blogFinder = async (req, res, next) => {
+	req.blog = await Blog.findByPk(req.params.id);
+	next();
+};
+
 router.get("/", async (req, res) => {
 	const blogs = await Blog.findAll();
 	res.json(blogs);
 });
 
-router.get("/:id", async (req, res) => {
-	const blog = await Blog.findByPk(req.params.id);
-	if (blog) {
-		res.json(blog);
+router.get("/:id", blogFinder, async (req, res) => {
+	if (req.blog) {
+		res.json(req.blog);
 	} else {
 		res.status(404).end();
 	}
@@ -25,25 +29,21 @@ router.post("/", async (req, res) => {
 	}
 });
 
-router.put("/:id", async (req, res) => {
-	const blog = await Blog.findByPk(req.params.id);
-	if (blog) {
-		blog.likes = req.body.likes || ++blog.likes;
-		await blog.save();
-		res.json(blog);
+router.put("/:id", blogFinder, async (req, res) => {
+	if (req.blog) {
+		req.blog.likes = req.body.likes || ++req.blog.likes;
+		await req.blog.save();
+		res.json(req.blog);
 	} else {
 		res.status(404).end();
 	}
 });
 
-router.delete("/:id", async (req, res) => {
-	try {
-		const id = req.params.id;
-		const blog = await Blog.destroy({ where: { id } });
-		return res.status(200);
-	} catch (error) {
-		return res.status(400).json({ error });
+router.delete("/:id", blogFinder, async (req, res) => {
+	if (req.blog) {
+		await req.blog.destroy();
 	}
+	res.status(204).end();
 });
 
 module.exports = router;
