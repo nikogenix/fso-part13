71d328b2cec043d9b4bdf0856eca2d986b6c374b
nikogenix <nikogenix@gmail.com>
2023-07-14 06:18:52 +0300

    Adding migration for reading list

--- blog-list-backend/models/index.js
+++ blog-list-backend/models/index.js
@@ -1,10 +1,15 @@
 const Blog = require("./blog");
 const User = require("./user");
+const ReadingList = require("./reading_list");
 
 Blog.belongsTo(User);
 User.hasMany(Blog);
 
+Blog.belongsToMany(User, { through: ReadingList });
+User.belongsToMany(Blog, { through: ReadingList });
+
 module.exports = {
 	Blog,
 	User,
+	ReadingList,
 };
---
+++ blog-list-backend/models/reading_list.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require("sequelize");
+
+const { sequelize } = require("../util/db");
+
+class ReadingList extends Model {}
+
+ReadingList.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		blogId: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: { model: "blogs", key: "id" },
+		},
+		userId: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: { model: "users", key: "id" },
+		},
+		read: {
+			type: DataTypes.BOOLEAN,
+			defaultValue: false,
+		},
+	},
+	{
+		sequelize,
+		underscored: true,
+		timestamps: false,
+		modelName: "team",
+	}
+);
+
+module.exports = ReadingList;
